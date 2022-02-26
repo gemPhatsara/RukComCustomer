6b07b0c1f477b66093e14491b4307e87b5eae7aa
Phatsarapawn Suealueang <gem.phatsara@gmail.com>
2022-02-26 23:16:27 +0700

    change file db

--- customer_db.sql
+++ customer_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2022 at 04:53 PM
+-- Generation Time: Feb 26, 2022 at 05:15 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
